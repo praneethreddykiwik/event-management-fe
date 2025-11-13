eff2f911191109bba0ed3b187729c5119297f1e1
AAdnan-cbc <adnan.s@criskasecurity.com>
2025-11-13 14:52:06 +0530

    Gateway page refactor

--- src/pages/GatewayPage/GatewayPage.jsx
+++ src/pages/GatewayPage/GatewayPage.jsx
@@ -1,33 +1,43 @@
 import styled from "styled-components";
-import { StyledSecButton ,StyledBaseButton} from "../../components/Styled/Buttons.styled";
-import Gateway from "../../assets/gateway-image/img.jpg";
+import {
+  StyledSecButton,
+  StyledBaseButton,
+} from "../../components/Styled/Buttons.styled";
+import {StyledHeadingBig} from "../../components/Styled/Typography.styled";
+import logo from "../../assets/gateway-images/eventz-logo.png";
+import Gateway from "../../assets/gateway-images/img.jpg";
 const GatewayPage = () => {
   return (
     <Container_parent>
       <Container_child1>
-          <LogoText>eventz</LogoText>
-          <Title>Your Gateway to <br /> Memorable Experiences</Title>
-          <Description>
-            no need to rummage through irrelevant halls <br /> anymore, we got You
-            covered. we send you <br /> relevant delas based off of your interest
-          </Description>
-          <Button_section>
-            <StyledSecButton>Skip</StyledSecButton>
-          <StyledBaseButton>Next</StyledBaseButton>
-          </Button_section>
+        <LogoText>
+          <LogoIcon src={logo} />
+          <StyledCstHeading>eventz</StyledCstHeading>
+        </LogoText>
+
+        <Title>
+          Your Gateway to <br /> Memorable Experiences
+        </Title>
+        <Description>
+          no need to rummage through irrelevant halls <br /> anymore, we got You
+          covered. we send you <br /> relevant delas based off of your interest
+        </Description>
+        <Button_section>
+          <Skip>Skip</Skip>
+          <Next>Next</Next>
+        </Button_section>
       </Container_child1>
-      <Container_child2> 
-      <ContainerImg src={Gateway} />
+      <Container_child2>
+        <ContainerImg src={Gateway} />
       </Container_child2>
     </Container_parent>
-  );                                                                    
+  );
 };
 export default GatewayPage;
 
 const ContainerImg = styled.img`
-height:100%;
-width:100%;
-
+  height: 100%;
+  width: 100%;
 `;
 const Container_parent = styled.div`
   display: flex;
@@ -58,35 +68,45 @@ const LogoRow = styled.div`
 `;
 
 const LogoIcon = styled.img`
-  width: 40px;
+  width: 80px;
 `;
 
-const LogoText = styled.span`
-  font-size: 28px;
-  font-weight: 600;
-  color: #0c0c0cff;
+const LogoText = styled.div`
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  margin-bottom: 60px;
 `;
 
 const Title = styled.h1`
-  font-size: 32px;
+  font-size: 35px;
   line-height: 1.3;
   font-weight: 700;
   color: #0a0a0a;
-  margin-bottom:0px;
+  margin-bottom: 0px;
 `;
 
 const Description = styled.p`
   font-size: 15px;
   color: #555;
   margin-bottom: 45px;
-
 `;
 
 const Button_section = styled.div`
   display: flex;
-  gap: 200px;
+  gap: 50px;
   align-items: center;
   justify-content: center;
-  margin-top:100px;
-  
+  margin-top: 100px;
 `;
+const Skip = styled(StyledSecButton)`
+  width: 150px;
+`;
+const Next = styled(StyledBaseButton)`
+  width: 250px;
+`;
+const StyledCstHeading = styled(StyledHeadingBig)`
+  font-weight: 300;
+
+`
