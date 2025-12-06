cbcfb7f86d07f62ee74f07f5c91733046e37bd9f
ABDUL WAHID SYED <abdulwahidsyed@Wahids-MacBook-Pro.local>
2025-12-06 10:05:43 +0530

    theme setup

--- src/theme/theme.jsx
+++ src/theme/theme.jsx
@@ -1,7 +1,27 @@
+const spacings = {
+  "spacing-1": "4px",
+  "spacing-2": "8px",
+};
+
+const typography = `
+  font-size: 10px
+`;
+
+const colors = `
+  primary: "#000"
+`;
+
+const borders = ``;
+
 export const theme = {
   light: {
     appBackgroundColor: "#fff",
     color: "#000",
+
+    spacings,
+    typography,
+    borders,
+    colors,
   },
   dark: {
     appBackgroundColor: "#000",
