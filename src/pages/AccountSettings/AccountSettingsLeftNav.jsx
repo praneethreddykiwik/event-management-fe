9f02d89977e7559c5af3e98d37b01794899949b9
Raghavendra <Raghavendra.Sunkara@criskasecurity.com>
2025-12-19 11:27:00 +0530

    account settings changes

--- src/pages/AccountSettings/AccountSettingsLeftNav.jsx
+++ src/pages/AccountSettings/AccountSettingsLeftNav.jsx
@@ -2,6 +2,14 @@ import React, { useState } from "react";
 import { HEADINGS, TEXTS } from "../../enum/accountsettings.common";
 import modalImg from "../../assets/modalimg.jpg";
 import {
+  StyledGroupIcon,
+  StyledProfileCard,
+  StyledProfileDivider,
+  StyledProfileHeader,
+  StyledProfileLabel,
+  StyledProfileRow,
+  StyledProfileSubtitle,
+  StyledProfileTitle,
   CloseButton,
   Item,
   List,
@@ -13,7 +21,6 @@ import {
   StyledLeftNav,
   StyledSemiHeadingAccount,
 } from "../../components/Styled/AccountSettings.styled";
-import AccountProfileSection from "./AccountProfileSection";
 
 const AccountSettingsLeftNav = () => {
   const [active, setActive] = useState("Account Settings");
@@ -31,7 +38,24 @@ const AccountSettingsLeftNav = () => {
 
   return (
     <StyledLeftNav>
-      <AccountProfileSection />
+      
+      <StyledProfileCard>
+        <StyledProfileHeader>
+          <StyledProfileTitle>Wade’s Profile</StyledProfileTitle>
+          <span className="material-symbols-outlined icon">keyboard_arrow_down</span>
+        </StyledProfileHeader>
+        <StyledProfileSubtitle>1 friend</StyledProfileSubtitle>
+
+        <StyledProfileDivider />
+
+        <StyledProfileRow>
+          <StyledGroupIcon className="material-symbols-outlined icon">
+            groups
+          </StyledGroupIcon>
+          <StyledProfileLabel>Invite friends</StyledProfileLabel>
+        </StyledProfileRow>
+      </StyledProfileCard>
+      
       <StyledSemiHeadingAccount>{HEADINGS.MY_ACCOUNT}</StyledSemiHeadingAccount>
       <Nav>
         <List>
