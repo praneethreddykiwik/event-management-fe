d0080d6c4d49063424c9d75f5b225979dd0bcb29
likithchodey <likith.chdey@criskasecurity.com>
2025-12-19 12:17:57 +0530

    added changes

--- src/components/PlayerCard/PlayerCard.jsx
+++ src/components/PlayerCard/PlayerCard.jsx
@@ -36,15 +36,7 @@ const PlayerCard = () => {
           <SideLeft>
             <SmallIconButton aria-label="Playlist">≡</SmallIconButton>
             <SmallIconButton aria-label="Switch event">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#e3e3e3"
-              >
-                <path d="M560-160v-80h104L537-367l57-57 126 126v-102h80v240H560Zm-344 0-56-56 504-504H560v-80h240v240h-80v-104L216-160Zm151-377L160-744l56-56 207 207-56 56Z" />
-              </svg>
+              <span class="material-symbols-outlined">open_in_full</span>
             </SmallIconButton>
           </SideLeft>
 
