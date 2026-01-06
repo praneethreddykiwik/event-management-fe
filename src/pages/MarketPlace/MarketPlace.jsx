4d96abeca1fb976035e96dc3962573ff6941299f
unknown <SAI-prasanna-cbc>
2026-01-06 22:08:28 +0530

    market_place

--- src/pages/MarketPlace/MarketPlace.jsx
+++ src/pages/MarketPlace/MarketPlace.jsx
@@ -1,14 +1,15 @@
 import { StyledHeading } from "../../components/Styled/Typography.styled";
 import { BlueBackHOC } from "../../HOC/BlueBackHOC";
 import { Venues } from "./Venues";
+import MarketDescription from "./MarketDescription";
 
 export const MarketPlace = () => {
   return (
     <BlueBackHOC>
-      <StyledHeading>Welcome to market place.</StyledHeading>
-      {/* <MarketDescription /> */}
+      
+      <MarketDescription />
       {/* <FeaturedEvents /> */}
-      <Venues />
+      {/* <Venues /> */}
     </BlueBackHOC>
   );
 };
