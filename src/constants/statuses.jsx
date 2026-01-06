eaf1999d4761e168d33af7cf57f466db20120797
ABDUL WAHID SYED <abdulwahidsyed@Wahids-MacBook-Pro.local>
2026-01-06 13:52:01 +0530

    charts

--- src/constants/statuses.jsx
+++ src/constants/statuses.jsx
@@ -2,94 +2,99 @@
 
 export const taskStatuses = {
   completed: {
-    badgeColor: 'completed',
-    icon: 'task_alt',
-    status: 'Completed',
+    badgeColor: "completed",
+    icon: "task_alt",
+    status: "Completed",
   },
 
   assigned: {
-    badgeColor: 'assigned',
-    icon: 'assignment_ind',
-    status: 'Assigned',
+    badgeColor: "assigned",
+    icon: "assignment_ind",
+    status: "Assigned",
   },
 
   not_started: {
-    badgeColor: 'notstarted',
-    icon: 'schedule',
-    status: 'Not Started',
+    badgeColor: "notstarted",
+    icon: "schedule",
+    status: "Not Started",
   },
 
   in_progress: {
-    badgeColor: 'inprogress',
-    icon: 'hourglass_bottom',
-    status: 'In Progress',
+    badgeColor: "inprogress",
+    icon: "hourglass_bottom",
+    status: "In Progress",
   },
 
   cancelled: {
-    badgeColor: 'cancelled',
-    icon: 'cancel',
-    status: 'Cancelled',
+    badgeColor: "cancelled",
+    icon: "cancel",
+    status: "Cancelled",
   },
 
   deleted: {
-    badgeColor: 'deleted',
-    icon: 'delete',
-    status: 'Deleted',
+    badgeColor: "deleted",
+    icon: "delete",
+    status: "Deleted",
   },
 };
 
 export const eventStatuses = {
   pending: {
-    badgeColor: 'pending',
-    icon: 'pending_actions',
-    status: 'Pending',
+    badgeColor: "pending",
+    icon: "pending_actions",
+    status: "Pending",
   },
 
   assigned: {
-    badgeColor: 'assigned',
-    icon: 'assignment_ind',
-    status: 'Assigned',
+    badgeColor: "assigned",
+    icon: "assignment_ind",
+    status: "Assigned",
   },
 
   accepted: {
-    badgeColor: 'completed',
-    icon: 'thumb_up',
-    status: 'Accepted',
+    badgeColor: "completed",
+    icon: "thumb_up",
+    status: "Accepted",
   },
 
   ready: {
-    badgeColor: 'active',
-    icon: 'task',
-    status: 'Ready',
+    badgeColor: "active",
+    icon: "task",
+    status: "Ready",
   },
 
   in_progress: {
-    badgeColor: 'inprogress',
-    icon: 'hourglass_bottom',
-    status: 'In Progress',
+    badgeColor: "inprogress",
+    icon: "hourglass_bottom",
+    status: "In Progress",
   },
 
   completed: {
-    badgeColor: 'completed',
-    icon: 'task_alt',
-    status: 'Completed',
+    badgeColor: "completed",
+    icon: "task_alt",
+    status: "Completed",
   },
 
   declined: {
-    badgeColor: 'declined',
-    icon: 'thumb_down',
-    status: 'Declined',
+    badgeColor: "declined",
+    icon: "thumb_down",
+    status: "Declined",
   },
 
   cancelled: {
-    badgeColor: 'cancelled',
-    icon: 'cancel',
-    status: 'Cancelled',
+    badgeColor: "cancelled",
+    icon: "cancel",
+    status: "Cancelled",
   },
 
   deleted: {
-    badgeColor: 'deleted',
-    icon: 'delete',
-    status: 'Deleted',
+    badgeColor: "deleted",
+    icon: "delete",
+    status: "Deleted",
   },
+};
+
+export const managersStatuses = {
+  IDLE: {},
+  DISABLED: {},
 };
