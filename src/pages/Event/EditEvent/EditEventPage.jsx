0042805b1878a8e9e2c811e2f4b523a1218f5683
navya0011-ux <Navya.Gujjeti@criskasecurity.com>
2026-01-23 17:33:11 +0530

    assignedToUid updated

--- src/pages/Event/EditEvent/EditEventPage.jsx
+++ src/pages/Event/EditEvent/EditEventPage.jsx
@@ -66,7 +66,7 @@ const CreateEvent = ({ onCreateEvent }) => {
       navigate,
       reqPayload: {
         ...reqPayload,
-        assigned_to_uid: reqPayload.assignedEventManager,
+        assignedToUid: reqPayload.assignedEventManager,
         tenantUid,
         scheduledAt,
         status: "pending",
