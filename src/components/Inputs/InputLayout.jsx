29e6a1188ec7d25ed9ebc74f33b3499f500f7d17
Sindhuja1908 <Sindhuja.Vallapureddy@criskasecurity.com>
2026-03-05 21:49:43 +0530

    .

--- src/components/Inputs/InputLayout.jsx
+++ src/components/Inputs/InputLayout.jsx
@@ -5,20 +5,10 @@ import {
 } from "../Styled/Typography.styled";
 import { StyledInputLayout } from "./Inputs.styled";
 
-<<<<<<< HEAD
-
-export const InputLayout = ({ label, error, width, ...props }) => {
-
-
-  return (
-    <StyledCtn style={{ flexBasis: width || "100%" }}>
-      {label ? <StyledParagraphSmall>{label} </StyledParagraphSmall> : null}
-=======
 export const InputLayout = ({ label, error, width, helperText, ...props }) => {
   return (
     <StyledInputLayout style={{ flexBasis: width || "100%" }}>
       {label ? <StyledParagraphSmall>{label}</StyledParagraphSmall> : null}
->>>>>>> 0f622fe8252655418a97eab39802edf16ffa27db
       {props.children}
       {helperText ? (
         <StyledAnchorSmall target="_blank" href={helperText} left>
